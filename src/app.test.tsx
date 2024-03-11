cff9cd7882ce9e5fc56222c856864c98d35797d5
vishalkokane264 <vishalkokane264@gmail.com>
2024-03-11 15:36:29 +0530

    sample test case added

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,3 +1,8 @@
-test('demo', () => {
-  expect(true).toBe(true)
+import App from './App'
+import { render, screen } from '@testing-library/react'
+
+test('renders App component', () => {
+  render(<App />)
+  const headingElement = screen.getByText(/Hello App component/i)
+  expect(headingElement).toBeTruthy()
 })
