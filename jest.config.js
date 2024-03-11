232189f54a33f9a02148f536fd478e892da1e524
vishalkokane264 <vishalkokane264@gmail.com>
2024-03-11 15:07:56 +0530

    jest configured

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
-module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.{js,jsx,tsx,ts}'],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'jsdom',
-}
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom',
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+      // process `*.tsx` files with `ts-jest`
+    },
+    rootDir: 'src',
+    moduleNameMapper: {
+      '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+      '^@app/(.*)$': '<rootDir>/$1',
+    },
+  };
