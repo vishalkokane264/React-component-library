8fd0448f494143594930b99948c27397ed0d6fdb
vishalkokane264 <vishalkokane264@gmail.com>
2024-03-11 16:06:55 +0530

    storybook configurations added

--- lib/Molecules/Page/Page.tsx
+++ lib/Molecules/Page/Page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import { Header } from './Header'
 import './page.css'
+import { Header } from './../../Atoms/Header/Header'
 
 type User = {
   name: string
---
+++ lib/index.ts
@@ -0,0 +1,9 @@
+//Export Nucleus
+
+//Export Atoms
+export * from './Atoms/Button/Button'
+export * from './Atoms/Header/Header'
+//Export Molecules
+export * from './Molecules/Page/Page'
+
+//Export Organisms
